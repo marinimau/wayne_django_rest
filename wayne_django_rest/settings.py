7c2b5ac5e194751c08e68721e20649483c0ef510
marinimau <mauro-marini97@gmail.com>
2020-03-14 12:30:43 +0100

    sistemato, è in ordine

--- wayne_django_rest/settings.py
+++ wayne_django_rest/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'user',
 ]
 
@@ -84,6 +85,13 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
     }
+}
+
+# Rest Framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 
 
