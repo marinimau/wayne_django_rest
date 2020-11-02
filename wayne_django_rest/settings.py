d060c7ef3e4bd3a2e0bbb33c9765d8e5c4168280
Mauro Marini <mauro.marini97@gmail.com>
2020-11-02 19:59:33 +0100

    clenup code

--- wayne_django_rest/settings.py
+++ wayne_django_rest/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'api.user',
     'api.password_recovery',
     'api.client_config',
@@ -109,7 +110,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
