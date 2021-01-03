9dd99bd5cdaa0010d637c1ec4be7230f0449f886
Mauro Marini <mauro.marini97@gmail.com>
2021-01-03 10:40:53 +0100

    update configuration

--- api/utils.py
+++ api/utils.py
@@ -10,7 +10,7 @@
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
-from django.core.mail import send_mail, send_mass_mail
+from django.core.mail import send_mail, send_mass_mail, EmailMessage
 from api.user.tokens import account_activation_token
 from django.conf import settings
 
@@ -28,22 +28,19 @@ def get_client_ip(request):
 
 
 def send_confirm_registration_email(user):
-    message = (
-        '{} - Activate Your Account'.format(settings.APP_NAME),
-        (render_to_string('email_templates/account_activation_email.html', {
+    message = EmailMessage(
+        subject='{} - Activate Your Account'.format(settings.APP_NAME),
+        body=(render_to_string('email_templates/account_activation_email.html', {
             'user': user,
             'domain': settings.SITE_URL,
             'uid': urlsafe_base64_encode(force_bytes(user.pk)),
             'token': account_activation_token.make_token(user),
             'appname': settings.APP_NAME
         })),
-        sender,
-        [user.email])
+        from_email=sender,
+        to=[user.email])
     print(message)
-    send_mass_mail(
-        message,
-        fail_silently=False,
-    )
+    message.send(fail_silently=False)
 
 
 def send_reset_password_email(user, ip, user_agent, token):
