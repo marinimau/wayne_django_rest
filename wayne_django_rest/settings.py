30faf6e400a5ca107dc1db29a01a8b5b93117bd1
marinimau <mauro.marini97@gmail.com>
2021-01-04 13:54:23 +0100

    update configuration

--- wayne_django_rest/settings.py
+++ wayne_django_rest/settings.py
@@ -131,8 +131,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_authtoken.auth.AuthTokenAuthentication',
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
